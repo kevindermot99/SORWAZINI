efb593e635aefdf1b3bab7d61383c356abf550bb
kevindermot99 <mbonimpayekevin@gmail.com>
2024-05-28 12:27:09 +0200

    content.js init

---
+++ src/Content/Content.js
@@ -0,0 +1,46 @@
+// Hero Texts
+export const HeroText = {
+  Big: `Building Tomorrow's Solutions Today`,
+  Small: `We strive to create innovative solutions today, shaping a better and brighter future for all.`,
+};
+
+export const Members = [
+  {
+    SO: [
+      {
+        name: "Lineo Sylvia Muso",
+        picture: "",
+      },
+    ],
+    RWA: [
+      {
+        name: "M. Kaneza Kevin",
+        picture: "",
+      },
+      {
+        name: "G. Rwema Christian",
+        picture: "",
+      },
+      {
+        name: "Anitha Uwimpuhwe",
+        picture: "",
+      },
+    ],
+    ZI: [
+      {
+        name: "T. Gift Marufu",
+        picture: "",
+      },
+    ],
+    NI: [
+      {
+        name: "C. Chinyeaka Eke",
+        picture: "",
+      },
+      {
+        name: "K. Honourgod Levison",
+        picture: "",
+      },
+    ],
+  },
+];
